f0e066f60af1fe36e1e3802cba689d7e57110766
Heng Li <lh3@me.com>
2015-04-13 09:25:43 -0400

    convert long deletions in various format to BED

---
+++ tex/del2bed.js
@@ -0,0 +1,67 @@
+var getopt = function(args, ostr) {
+	var oli; // option letter list index
+	if (typeof(getopt.place) == 'undefined')
+		getopt.ind = 0, getopt.arg = null, getopt.place = -1;
+	if (getopt.place == -1) { // update scanning pointer
+		if (getopt.ind >= args.length || args[getopt.ind].charAt(getopt.place = 0) != '-') {
+			getopt.place = -1;
+			return null;
+		}
+		if (getopt.place + 1 < args[getopt.ind].length && args[getopt.ind].charAt(++getopt.place) == '-') { // found "--"
+			++getopt.ind;
+			getopt.place = -1;
+			return null;
+		}
+	}
+	var optopt = args[getopt.ind].charAt(getopt.place++); // character checked for validity
+	if (optopt == ':' || (oli = ostr.indexOf(optopt)) < 0) {
+		if (optopt == '-') return null; //  if the user didn't specify '-' as an option, assume it means null.
+		if (getopt.place < 0) ++getopt.ind;
+		return '?';
+	}
+	if (oli+1 >= ostr.length || ostr.charAt(++oli) != ':') { // don't need argument
+		getopt.arg = null;
+		if (getopt.place < 0 || getopt.place >= args[getopt.ind].length) ++getopt.ind, getopt.place = -1;
+	} else { // need an argument
+		if (getopt.place >= 0 && getopt.place < args[getopt.ind].length)
+			getopt.arg = args[getopt.ind].substr(getopt.place);
+		else if (args.length <= ++getopt.ind) { // no arg
+			getopt.place = -1;
+			if (ostr.length > 0 && ostr.charAt(0) == ':') return ':';
+			return '?';
+		} else getopt.arg = args[getopt.ind]; // white space
+		getopt.place = -1;
+		++getopt.ind;
+	}
+	return optopt;
+}
+
+var c, show_filtered = false;
+
+while ((c = getopt(arguments, "f")) != null) {
+	if (c == 'f') show_filtered = true;
+}
+
+var file = arguments.length == getopt.ind? new File() : new File(arguments[getopt.ind]);
+var buf = new Bytes();
+
+while (file.readline(buf) >= 0) {
+	var line = buf.toString();
+	var m, t = line.split("\t");
+	if (t.length >= 6 && /\tTYPE:DELETION/.test(line) && (m = /\tMAX:([^:\s]+):(\d+);([^:\s]+):(\d+)/.exec(line)) != null) { // bedpe produced by LUMPY
+		if (m[1] == m[3]) print(m[1], parseInt(m[2])-1, m[4], t[t.length-1]);
+		else warn("WARNING: different contig names");
+	} else if (line.charAt(0) != '#' && t.length > 8 && /^\d+$/.test(t[1]) && (m = /[;\t]END=(\d+)/.exec(t[7])) != null) { // VCF
+		if (t.length >= 10 && /^0[\/|]0/.test(t[9])) continue; // 0/0 genotype
+		if (/SVTYPE=DEL/.test(t[7]) || /<DEL>/.test(t[4])) {
+			if (show_filtered || t[6] == 'PASS' || t[6] == '.')
+				print(t[0], parseInt(t[1])-1, m[1], t[6]);
+		}
+	} else if (t.length >= 6 && t[0] == 'D' && /^\d+/.test(t[2]) && /^\d+/.test(t[3])) { // htsbox abreak
+		for (var i = 2; i <= 5; ++i) t[i] = parseInt(t[i]); 
+		print(t[1], t[2]<t[3]+t[4]? t[2]:t[3]+t[4], t[3]>t[2]-t[4]?t[3]:t[2]-t[4]);
+	}
+}
+
+buf.destroy();
+file.close();
