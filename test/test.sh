5555f2ed9afee83b63bc207dac4a6b23237940d5
Heng Li <lh3@me.com>
2015-07-23 08:02:07 -0400

    enabled run-calling; made work on Mac

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@ cd "$HERE"
 rm -f ARTIFACT_*
 
 function unitig_lengths {
-	zcat "$1" | awk '{if(NR%4==2) print length($1)}' | sort -nr
+	gzip -dc "$1" | awk '{if(NR%4==2) print length($1)}' | sort -nr
 }
 function N50 {
 	local L=$(unitig_lengths "$1" | awk 'BEGIN {tot=0;} {tot+=$0} END {print tot;}')
@@ -18,7 +18,7 @@ function N50 {
 # individual with a complex rearrangement relative to the indicated region of
 # the GRCh37 reference. It's described in Figure 7 of English et al. 2015
 # (doi:10.1186/s12864-015-1479-3).
-../fermi.kit/fermi2.pl unitig -s30000 -t$(nproc) -l100 -p ARTIFACT_HS1011_rearrangement HS1011_11_1910000_1940000.fastq.gz > ARTIFACT_HS1011_rearrangement.mak
+../fermi.kit/fermi2.pl unitig -s30000 -t4 -l100 -p ARTIFACT_HS1011_rearrangement HS1011_11_1910000_1940000.fastq.gz > ARTIFACT_HS1011_rearrangement.mak
 make -f ARTIFACT_HS1011_rearrangement.mak
 if [ $(N50 ARTIFACT_HS1011_rearrangement.mag.gz) -lt 2300 ]; then
 	echo "HS1011_rearrangement N50 too low!"
@@ -27,8 +27,7 @@ fi
 
 cp 11_1910000_1940000.fa ARTIFACT_11_1910000_1940000.fa
 ../fermi.kit/bwa index ARTIFACT_11_1910000_1940000.fa
-# FIXME: Segfaults on: htsbox abreak -cu ARTIFACT_HS1011_rearrangement.unsrt.sam.gz !
-#../fermi.kit/run-calling -t$(nproc) ARTIFACT_11_1910000_1940000.fa ARTIFACT_HS1011_rearrangement.mag.gz | bash -ex -o pipefail
+../fermi.kit/run-calling ARTIFACT_11_1910000_1940000.fa ARTIFACT_HS1011_rearrangement.mag.gz | bash -ex -o pipefail
 
 # TODO: look for evidence of breakpoints in ARTIFACT_HS1011_rearrangement.srt.bam
 # e.g.:
